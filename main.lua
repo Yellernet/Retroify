7feb7c6fd2983a4fb623cebc681ede27d3f1117c
Yellernet <lugowskiadam80@gmail.com>
2025-01-13 15:27:52 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,33 @@
 -- Gui to Lua
 -- Version: 3.2
+
+game.CoreGui.RoactAppExperimentProvider.Children.OffsetFrame.PlayerScrollList.SizeOffsetFrame.TopRoundedRect.DismissIconFrame.UICorner.CornerRadius = UDim.new(0,3)
+game.CoreGui.RoactAppExperimentProvider.Children.OffsetFrame.PlayerScrollList.SizeOffsetFrame.TopRoundedRect.DismissIconFrame.DismissButton:Destroy()
+game.CoreGui.TopBarApp.MenuIconHolder.TriggerPoint.Background.ScalingIcon.ImageTransparency = 1
+
+game.CoreGui.RoactAppExperimentProvider.Children.OffsetFrame.PlayerScrollList.SizeOffsetFrame.TopRoundedRect.DismissIconFrame.UICorner.CornerRadius = UDim.new(0,3)
+game.CoreGui.RoactAppExperimentProvider.Children.OffsetFrame.PlayerScrollList.SizeOffsetFrame.BottomRoundedRect["1"]["1"].CornerRadius = UDim.new(0,3)
+local yo = game.CoreGui.RoactAppExperimentProvider.Children.OffsetFrame.PlayerScrollList.SizeOffsetFrame.TopRoundedRect.Size.Y.Offset
+local ys = game.CoreGui.RoactAppExperimentProvider.Children.OffsetFrame.PlayerScrollList.SizeOffsetFrame.TopRoundedRect.Size.Y.Scale
+local xs = game.CoreGui.RoactAppExperimentProvider.Children.OffsetFrame.PlayerScrollList.SizeOffsetFrame.TopRoundedRect.Size.X.Scale
+local xo = game.CoreGui.RoactAppExperimentProvider.Children.OffsetFrame.PlayerScrollList.SizeOffsetFrame.TopRoundedRect.Size.X.Offset
+game.CoreGui.RoactAppExperimentProvider.Children.OffsetFrame.PlayerScrollList.SizeOffsetFrame.TopRoundedRect.Size = UDim2.new(xs,xo,ys,5)
+game.CoreGui.RoactAppExperimentProvider.Children.OffsetFrame.PlayerScrollList.SizeOffsetFrame.TopRoundedRect.BackgroundColor3 = Color3.new(0.839216, 0.835294, 0.847059)
+local function UpdatePlayerList()
+	game.CoreGui.RoactAppExperimentProvider.Children.OffsetFrame.PlayerScrollList.SizeOffsetFrame.TopRoundedRect.DismissIconFrame.BackgroundTransparency = 0.35
+	game.CoreGui.RoactAppExperimentProvider.Children.OffsetFrame.PlayerScrollList.SizeOffsetFrame.BottomRoundedRect["1"].BackgroundTransparency = 0.35
+	game.CoreGui.RoactAppExperimentProvider.Children.OffsetFrame.PlayerScrollList.SizeOffsetFrame.ScrollingFrameContainer.BackgroundTransparency = 0.35
+end
+
+
+coroutine.wrap(function()
+	while true do
+		UpdatePlayerList()
+		wait(0.03)
+	end
+
+
+
 local wat = game:GetService("Players").LocalPlayer.PlayerGui:FindFirstChild("EarlyCoreGui")
 if wat then
 	wat:Destroy()
